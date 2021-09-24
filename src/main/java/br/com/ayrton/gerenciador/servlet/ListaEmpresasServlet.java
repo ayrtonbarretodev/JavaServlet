93e81bed5716fb2aa2d3f58a15342100992aec39
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-09-24 09:43:20 -0300

    Excluindo o código HTML, chamando a página JSP de listar as empresas e setando a lista de empresas

--- src/main/java/br/com/ayrton/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/main/java/br/com/ayrton/gerenciador/servlet/ListaEmpresasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,14 +27,11 @@ public class ListaEmpresasServlet extends HttpServlet {
 		Banco banco = new Banco();
 		List<Empresa> listaEmpresas = banco.getEmpresas();
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
-		out.println("<ul>");
-		for (Empresa empresa : listaEmpresas) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		out.println("</ul>");
-		out.println("</body> <html>");
+		//chamar o JSP para mostrar os dados no navegador
+			RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+			request.setAttribute("listaEmpresas",listaEmpresas);
+			rd.forward(request, response);
+		
 	}
 
 }
