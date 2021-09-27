0d9d8adfec71afd9e8635797f520700840958218
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-09-26 21:16:00 -0300

    enviando a data para a página JSP

--- src/main/java/br/com/ayrton/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/ayrton/gerenciador/servlet/NovaEmpresaServlet.java
@@ -2,6 +2,9 @@ package br.com.ayrton.gerenciador.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -23,9 +26,20 @@ public class NovaEmpresaServlet extends HttpServlet {
 		
 		//o método getParameter sempre retorna uma String
 		String nomeDaEmpresa = req.getParameter("nome"); //lendo um parâmetro a partir da requisição
+		String paramDataEmpresa = req.getParameter("data");
+		
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
 		
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeDaEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		Banco banco = new Banco();
 		
