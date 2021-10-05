b09bb2f65d9ef0d7803d2b71494dd14ccb4ffddd
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-10-05 09:02:06 -0300

    criando servlet responsável por receber todas as requisições

---
+++ src/main/java/br/com/ayrton/gerenciador/servlet/UnicaEntradaServlet.java
@@ -0,0 +1,44 @@
+package br.com.ayrton.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.ayrton.gerenciador.acao.AlteraEmpresa;
+import br.com.ayrton.gerenciador.acao.ListaEmpresas;
+import br.com.ayrton.gerenciador.acao.MostraEmpresa;
+import br.com.ayrton.gerenciador.acao.NovaEmpresa;
+import br.com.ayrton.gerenciador.acao.RemoveEmpresa;
+
+
+@WebServlet("/entrada")
+public class UnicaEntradaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String paramAcao = request.getParameter("acao");
+		
+		if (paramAcao.equals("ListaEmpresas")) {
+			ListaEmpresas acao = new ListaEmpresas();
+			acao.executa(request, response);
+		}else if (paramAcao.equals("RemoveEmpresa")) {
+			RemoveEmpresa acao = new RemoveEmpresa();
+			acao.remove(request, response);
+		}else if(paramAcao.equals("MostraEmpresa")) {
+			MostraEmpresa acao = new MostraEmpresa();
+			acao.mostrarEmpresa(request, response);
+		}else if(paramAcao.equals("NovaEmpresa")) {
+			NovaEmpresa acao = new NovaEmpresa();
+			acao.novaEmpresa(request, response);
+		}else if(paramAcao.equals("AlteraEmpresa")) {
+			AlteraEmpresa acao = new AlteraEmpresa();
+			acao.alteraEmpresa(request, response);
+		}
+
+	}
+}
